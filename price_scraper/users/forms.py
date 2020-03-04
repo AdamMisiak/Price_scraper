c830fd72205e452ba05d505185db2f925b95a864
AdamMisiak <adammisiak3@gmail.com>
2020-03-04 17:36:28 +0100

    login and register tests added

--- price_scraper/users/forms.py
+++ price_scraper/users/forms.py
@@ -1,23 +1,24 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms import ValidationError
-from wtforms.validators import DataRequired,Email,EqualTo
+from wtforms.validators import DataRequired, Email, EqualTo
 from price_scraper.models import User
 
 
 class RegistrationForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired(), EqualTo('password_confirm', message='Passwords Must Match!')])
+    password = PasswordField('Password', validators=[DataRequired(), EqualTo('password_confirm',
+                                                                             message='Passwords Must Match!')])
     password_confirm = PasswordField('Confirm password', validators=[DataRequired()])
     submit = SubmitField('Register!')
 
     def validate_email(self, field):
-        if User.query.filter_by(email=field.data).first() == True:
+        if User.query.filter_by(email=field.data).first():
             raise ValidationError('Email you have chosen is already taken!')
 
     def validate_username(self, field):
-        if User.query.filter_by(username=field.data).first()  == True:
+        if User.query.filter_by(username=field.data).first():
             raise ValidationError('Username you have chosen is already taken!')
 
 class LoginForm(FlaskForm):
