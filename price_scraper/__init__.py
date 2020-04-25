155b553387087c0e6f3ff10f906126f69c2598f4
AdamMisiak <adammisiak3@gmail.com>
2020-04-25 11:53:28 +0200

    docker-compose added

--- price_scraper/__init__.py
+++ price_scraper/__init__.py
@@ -14,35 +14,34 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 app.config['SECURITY_PASSWORD_SALT'] = 'my_precious_two'
 
-# mail settings
 app.config.update(dict(
-    MAIL_SERVER='smtp.gmail.com',
-    MAIL_PORT=465,
-    MAIL_USE_TLS=False,
-    MAIL_USE_SSL=True,
-    MAIL_USERNAME='test.flask369@gmail.com',
-    MAIL_PASSWORD='Testflask369'
+	MAIL_SERVER='smtp.gmail.com',
+	MAIL_PORT=465,
+	MAIL_USE_TLS=False,
+	MAIL_USE_SSL=True,
+	MAIL_USERNAME='test.flask369@gmail.com',
+	MAIL_PASSWORD='Testflask369'
 ))
 
-# mail accounts
 app.config['MAIL_DEFAULT_SENDER'] = 'test.flask369@gmail.com'
 
 db = SQLAlchemy(app)
-migrate = Migrate(app,db)
+migrate = Migrate(app, db)
 mail = Mail(app)
-
 
 login_manager = LoginManager()
 login_manager.init_app(app)
 
+
 @login_manager.user_loader
 def load_user(user_id):
-    return None
-login_manager.login_view = "users.login"
+	return None
 
+
+login_manager.login_view = "users.login"
 
 from price_scraper.assets.views import assets_blueprint
 from price_scraper.users.views import users_blueprint
 
-app.register_blueprint(assets_blueprint,url_prefix='/assets')
-app.register_blueprint(users_blueprint,url_prefix='/users')
+app.register_blueprint(assets_blueprint, url_prefix='/assets')
+app.register_blueprint(users_blueprint, url_prefix='/users')
